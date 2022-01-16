b1458d67c152412b7da1fdc4a1dc1ce3845ae2ce
ptrpengdev <69882989+ptrpengdev@users.noreply.github.com>
2022-01-16 00:04:33 -0500

    Update README

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,3 @@
-// import { applyMiddleware, createStore, compose } from 'redux';
-// import thunkMiddleware from 'redux-thunk';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-// import rootReducer from '../reducers/index';
-
-// export function configureStore(preloadedState) {
-//   const middlewares = [thunkMiddleware];
-
-//   const middlewareEnhancer = composeWithDevTools(
-//     applyMiddleware(...middlewares)
-//   );
-
-//   const enhancers = [middlewareEnhancer];
-//   const composedEnhancers = compose(...enhancers);
-
-//   const store = createStore(rootReducer, preloadedState, composedEnhancers);
-
-//   return store;
-// }
 import { applyMiddleware, createStore } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
